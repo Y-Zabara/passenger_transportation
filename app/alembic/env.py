78276ffef63e88c19a3176d5cf0ae7de19f603ed
Yaroslav <zabaraaroslav@gmail.com>
2025-09-26 12:44:22 +0300

    Configurate alembic

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -7,6 +7,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from settings import config as app_config
+from core.models import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,13 +23,15 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+# set main db url
+config.set_main_option("sqlalchemy.url", app_config.db.url)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,11 +1,18 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
     MappedColumn,
 )
 
+from settings import config
+
 
 class Base(DeclarativeBase):
     __abstract__: bool = True
+
+    metadata = MetaData(
+        naming_convention=config.db.naming_convention,
+    )
 
     id: Mapped[int] = MappedColumn(primary_key=True)
