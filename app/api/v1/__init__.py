b2a9005234c7c6b4bdaa5e961ae17a1d4713dec4
Yaroslav <zabaraaroslav@gmail.com>
2025-09-27 19:16:32 +0300

    add pyjwt

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .items import router as item_router
 from .requests import router as requests_router
 from .users import router as users_router
+from .auth import router as auth_router
 
 v1_router = APIRouter(
     prefix="/v1",
@@ -10,3 +11,4 @@ v1_router = APIRouter(
 v1_router.include_router(item_router)
 v1_router.include_router(requests_router)
 v1_router.include_router(users_router)
+v1_router.include_router(auth_router)
